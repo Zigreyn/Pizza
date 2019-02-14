d94f876acbdd617db369e23253a50d43546d5ec6
Dmitry Nesterenko <dmitriy.nesterenko32@gmail.com>
2019-02-14 22:15:12 +0300

    add recycler list in Pizza

--- app/src/main/java/com/example/pizza/PizzaFragment.java
+++ app/src/main/java/com/example/pizza/PizzaFragment.java
@@ -2,21 +2,32 @@ package com.example.pizza;
 
 
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 
-public class  PizzaFragment extends ListFragment {
+public class  PizzaFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(inflater.getContext(),
-                android.R.layout.simple_list_item_1,
-                getResources().getStringArray(R.array.pizzas));
-        setListAdapter(adapter);
-        return super.onCreateView(inflater, container, savedInstanceState);
+
+        RecyclerView pizzaRecyclerView = (RecyclerView) inflater.inflate(R.layout.fragment_pizza, container, false);
+        String pizzaNames[] = new String[Pizza.pizzas.length];
+        int pizzaImg[] = new int[Pizza.pizzas.length];
+
+        for(int i = 0; i < Pizza.pizzas.length; i++){
+            pizzaNames[i] = Pizza.pizzas[i].getName();
+            pizzaImg[i] = Pizza.pizzas[i].getImageResourceId();
+        }
+
+        CaptionedImagesAdapter captionedImagesAdapter = new CaptionedImagesAdapter(pizzaNames, pizzaImg);
+        pizzaRecyclerView.setAdapter(captionedImagesAdapter);
+        GridLayoutManager layoutManager = new GridLayoutManager(getActivity(), 2);
+        pizzaRecyclerView.setLayoutManager(layoutManager);
+        return pizzaRecyclerView;
     }
 }
